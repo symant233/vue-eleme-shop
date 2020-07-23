c1af7878b22ea9df7aba3f21ec364efd594b8127
symant233 <symant233@gmail.com>
2020-07-23 11:56:07 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app" @touchmove.prevent>
+    <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper"></div>
+  </div>
+</template>
+
+<script>
+import qs from 'query-string'
+import { getSeller } from './api'
+import VHeader from 'components/v-header/v-header'
+
+export default {
+  data() {
+    return {
+      seller: {
+        id: qs.parse(location.search).id,
+      },
+    }
+  },
+  computed: {
+    tabs() {
+      return [
+        // {
+        //   label: '商品',
+        //   component: Goods,
+        //   data: {
+        //     seller: this.seller,
+        //   },
+        // },
+        // {
+        //   label: '评论',
+        //   component: Ratings,
+        //   data: {
+        //     seller: this.seller,
+        //   },
+        // },
+        // {
+        //   label: '商家',
+        //   component: Seller,
+        //   data: {
+        //     seller: this.seller,
+        //   },
+        // },
+      ]
+    },
+  },
+  created() {
+    this._getSeller()
+  },
+  methods: {
+    _getSeller() {
+      getSeller({
+        id: this.seller.id,
+      }).then((seller) => {
+        this.seller = Object.assign({}, this.seller, seller)
+      })
+    },
+  },
+  components: {
+    VHeader,
+  },
+}
+</script>
+
+<style lang="stylus">
+@import "~common/stylus/index"
+
+#app
+  .tab-wrapper
+    position: fixed
+    top: 136px
+    left: 0
+    right: 0
+    bottom: 0
+</style>
